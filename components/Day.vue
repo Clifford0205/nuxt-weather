98715dc96930e82e0923ff290ccd16456f925a75
Clifford <clifford@twjoin.com>
2021-12-22 20:15:02 +0800

    feat(pie-chart): 圓餅圖完成

--- components/Day.vue
+++ components/Day.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="col-five day">
+    <div class="date">
+      {{ day.applicable_date }}
+    </div>
     <img
       :src="`https://www.metaweather.com/static/img/weather/${weatherAbbr}.svg`"
       alt="天氣圖片"
     />
+    <div class="chart-area">
+      <figure class="pie-chart" v-bind="gradientStyle">
+        <figcaption class="pie-chart-content">
+          <p class="pie-chart-content-title">Humidity</p>
+          <p class="pie-chart-content-num">{{ humidity }}%</p>
+        </figcaption>
+      </figure>
+    </div>
   </div>
 </template>
 
@@ -23,6 +34,17 @@ export default {
   computed: {
     weatherAbbr() {
       return this.day.weather_state_abbr;
+    },
+    humidity() {
+      return this.day.humidity;
+    },
+
+    gradientStyle() {
+      return {
+        style: {
+          background: `conic-gradient(#5d96f7 0% ${this.humidity}%, #aaaaaa 0% 100%)`,
+        },
+      };
     },
   },
 };
